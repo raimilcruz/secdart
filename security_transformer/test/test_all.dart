1840746b3d7ce25b3dd271b149259547ea88e400
Raimil Cruz <raimil.cruz@gmail.com>
2018-04-18 09:44:16 -0300

    Add analysis_options file to the transformer project

---
+++ security_transformer/test/test_all.dart
@@ -0,0 +1,9 @@
+import 'transformer_test.dart' as transformer;
+
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+void main() {
+  defineReflectiveSuite(() {
+    transformer.main();
+  });
+}
