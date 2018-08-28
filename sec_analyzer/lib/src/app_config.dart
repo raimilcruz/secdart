d8b6a5c6a20c3fdefff632ede842c756335fb7ae
Raimil Cruz <raimil.cruz@gmail.com>
2018-08-28 10:42:17 -0300

    Default debug configuration when there is no config.yaml

--- sec_analyzer/lib/src/app_config.dart
+++ sec_analyzer/lib/src/app_config.dart
@@ -5,16 +5,22 @@ import 'package:safe_config/safe_config.dart';
 class AppConfiguration extends Configuration {
   static AppConfiguration _singleton;
 
-  AppConfiguration._internal(String fileName) :
-        super.fromFile(new File(fileName));
+  AppConfiguration._internal(File file) :
+        super.fromFile(file);
+
+  AppConfiguration._default() : super();
 
   int debug;
 
   bool get isDebug => debug == 1;
 
   static AppConfiguration defaultConfig(){
-    if(_singleton ==null)
-      _singleton = new AppConfiguration._internal("config.yaml");
+    if(_singleton ==null) {
+      var file = new File("config.yaml");
+      _singleton = file.existsSync()
+          ? new AppConfiguration._internal(file)
+          : new AppConfiguration._default();
+    }
     return _singleton;
   }
 }
